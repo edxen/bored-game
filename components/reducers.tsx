fc3bd2b14a92e1e7589fd62cf6768ab3c82a4b6c
edxen <edabogajr@gmail.com>
2023-12-18 05:00:31 +0800

    declare only top level state in root reducer

--- components/reducers.tsx
+++ components/reducers.tsx
@@ -4,19 +4,13 @@ import diceReducer from "./reducers/diceReducer";
 import tilesReducer from "./reducers/tilesReducer";
 import playersReducer from "./reducers/playersReducer";
 import turnsReducer from "./reducers/turnReducer";
-export type RootState = ReturnType<typeof rootReducer>;
 
 const rootReducer = combineReducers({
     turns: turnsReducer,
-    setTurn: turnsReducer,
-    nextTurn: turnsReducer,
     dice: diceReducer,
-    setDice: diceReducer,
     tiles: tilesReducer,
-    setTile: tilesReducer,
     players: playersReducer,
-    setPlayer: playersReducer,
-    setPlayers: playersReducer
 });
 
+export type RootState = ReturnType<typeof rootReducer>;
 export default rootReducer;
