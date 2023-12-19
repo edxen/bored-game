09441df12b716bd6b991fe8dde08064e81ecdf85
edxen <edabogajr@gmail.com>
2023-12-20 05:57:53 +0800

    refactor generateTiles and separate assignTypeToPath to prep for other types

--- components/reducers/tilesReducer.tsx
+++ components/reducers/tilesReducer.tsx
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import generateTiles from "../utils/generateTiles";
 
 export type TTile = {
     type: 'plain' | 'portal',
@@ -6,44 +7,6 @@ export type TTile = {
     edge: boolean;
     path: number;
     index: number;
-};
-
-const generateTiles = (columns: number, rows: number): TTile[] => {
-    return Array.from({ length: columns * rows }, (_, index) => {
-        const row = Math.floor(index / columns);
-        const column = index % columns;
-        const isEdge = row === 0 || row === rows - 1 || column === 0 || column === columns - 1;
-
-        const getPath = (): number => {
-            if (row === 0) {
-                return column + 1;
-            } else if (column === columns - 1 && row !== rows - 1) {
-                return columns + row;
-            } else if (row === rows - 1 && column !== 0) {
-                return columns * 2 + (columns - column) - 2;
-            } else if (column === 0) {
-                return (columns * 3) + (rows - row) - 3;
-            }
-            return 0;
-        };
-
-        let type: TTile['type'] = 'plain';
-
-        const setPortalTiles = () => {
-            if ([6, 15, 24, 33].includes(getPath())) {
-                type = 'portal';
-            }
-        };
-        setPortalTiles();
-
-        return {
-            index,
-            type,
-            occupants: [],
-            edge: isEdge,
-            path: isEdge ? getPath() : 0
-        };
-    });
 };
 
 interface TileState extends Array<TTile> { }
@@ -56,7 +19,7 @@ export type TilePayload<K extends keyof TTile> = {
 
 const tileSlice = createSlice({
     name: 'tiles',
-    initialState: generateTiles(10, 10),
+    initialState: generateTiles({ columns: 10, rows: 10 }),
     reducers: {
         setTile: <K extends keyof TTile>(state: TileState, action: PayloadAction<TilePayload<K>>) => {
             const { index, key, value } = action.payload;
---
+++ components/utils/generateTiles.tsx
@@ -0,0 +1,59 @@
+import { TTile } from "../reducers/tilesReducer";
+
+type TNumberParams = { [key: string]: number; };
+
+// in a setting where columns is 10 and rows is 10, i get 100 tiles
+// tile line will be decided from first to last minus 1
+// for ex. 
+// top edge     = 1-9
+// right edge   = 10-18
+// bottom edge  = 19-27
+// left edge    = 28-36
+
+const getPath = ({ row, rows, column, columns }: TNumberParams): number => { // Get back here to refactor for accomodate different vector size
+    const isTop = () => row === 0;
+    const isRight = () => column === columns - 1 && row !== rows - 1;
+    const isBottom = () => row === rows - 1 && column !== 0;
+    const isLeft = () => column === 0;
+
+    let path = 0;
+    if (isTop()) path = column + 1;
+    else if (isRight()) path = columns + row;
+    else if (isBottom()) path = columns * 2 + (columns - column) - 2;
+    else if (isLeft()) path = (columns * 3) + (rows - row) - 3;
+    return path;
+};
+
+type TTileType = TTile['type'];
+const assignTypeToPath = (path: number): TTileType => {
+    let type: Partial<TTileType> = 'plain';
+
+    const pathSet: Record<Exclude<TTileType, 'plain'>, number[]> = {
+        portal: [6, 15, 24, 33]
+    };
+
+    Object.entries(pathSet).forEach(([key, value]) => {
+        if (value.includes(path)) type = key as TTileType;
+    });
+
+    return type;
+};
+
+const generateTiles = ({ columns, rows }: TNumberParams): TTile[] => {
+    return Array.from({ length: columns * rows }, (_, index) => {
+        const row = Math.floor(index / columns);
+        const column = index % columns;
+        const edge = row === 0 || row === rows - 1 || column === 0 || column === columns - 1;
+        const path = edge ? getPath({ row, rows, column, columns }) : 0;
+
+        return {
+            index,
+            edge,
+            path,
+            type: assignTypeToPath(path),
+            occupants: []
+        };
+    });
+};
+
+export default generateTiles;
