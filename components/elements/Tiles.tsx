48efe92bfbf58925eb2b9a90839bd099bb164442
edxen <edabogajr@gmail.com>
2023-12-20 06:37:51 +0800

    fix active player indicator not displaying

--- components/elements/Tiles.tsx
+++ components/elements/Tiles.tsx
@@ -4,7 +4,7 @@ import GenerateTileWithIcon from "./tiles/GenerateTileWithIcon";
 function Tiles() {
     const { turns, tiles, getPlayerData } = GetData();
 
-    let tileClass = 'relative flex justify-center items-center min-w-[2.5rem] w-10 h-10 border border-slate-100';
+    let tileClass = 'relative flex justify-center items-center min-w-[2.5rem] w-10 h-10 border';
 
     const renderOccupants = (occupants: string[]) => {
         return occupants.map((occupant) => {
@@ -13,7 +13,7 @@ function Tiles() {
                 const { id, color, name } = playerData;
 
                 return (
-                    <div className={`${tileClass} ${color} ${turns.players.length && turns.players[0].id === id ? 'border-4 border-red-800' : ''} text-white z-10`} key={id}>
+                    <div className={`${tileClass} ${color} ${turns.players.length && turns.players[0].id === id ? 'border-4 border-red-800' : 'border-slate-100'} text-white z-10`} key={id}>
                         {name}
                     </div>
                 );
