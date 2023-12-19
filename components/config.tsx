e45d82b8bddd7f03f2039e6c06af017db077bed1
edxen <edabogajr@gmail.com>
2023-12-19 09:40:52 +0800

    added config for customerPlayers

--- components/config.tsx
+++ components/config.tsx
@@ -1,13 +1,23 @@
 import { TPlayer } from "./reducers/playersReducer";
 
+const humanvscpu = [
+    { id: 'playera', type: 'human', name: 'P1', path: 1, color: 'bg-red-700' },
+    { id: 'playerb', type: 'computer', name: 'P2', path: 7, color: 'bg-blue-700' }
+];
+
+const allhumans = [
+    { id: 'playera', type: 'human', name: 'P1', path: 1, color: 'bg-red-700' },
+    { id: 'playerb', type: 'human', name: 'P2', path: 10, color: 'bg-blue-700' },
+    { id: 'playerc', type: 'human', name: 'P3', path: 19, color: 'bg-orange-700' },
+    { id: 'playerd', type: 'human', name: 'P4', path: 28, color: 'bg-green-700' },
+
+];
+
 const config = {
-    enableSpecificDice: false,
+    enableSpecificDice: true,
     customPlayer: {
-        enabled: false,
-        state: [
-            { id: 'playera', type: 'human', name: 'P1', path: 1, color: 'bg-red-700' },
-            { id: 'playerb', type: 'computer', name: 'P2', path: 7, color: 'bg-blue-700' }
-        ] as TPlayer[]
+        enabled: true,
+        state: humanvscpu as TPlayer[]
     }
 };
 
