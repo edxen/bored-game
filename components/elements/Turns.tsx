97bc815f15d94e0ab57487836c3adc52e4fab586
edxen <edabogajr@gmail.com>
2023-12-22 12:05:32 +0800

    remove early return for Turns component

--- components/elements/Turns.tsx
+++ components/elements/Turns.tsx
@@ -5,12 +5,11 @@ const Turns = () => {
     const { round } = game;
 
     const queuePlayers = round.queue.map(id => getPlayerData(id));
-    if (!queuePlayers.length) return;
 
     return (
         <div className='flex justify-center items-center gap-2 my-2'>
             <div className='font-bold'>Round {round.count} | Turns {round.turn} :</div>
-            {queuePlayers.map((player, i) => player && (
+            {queuePlayers?.map((player, i) => player && (
                 <div className='flex gap-2' key={player.id}>
                     <div>{'>>'}</div>
                     <div className={i === 0 ? `font-bold underline` : ''}>{player.name}</div>
