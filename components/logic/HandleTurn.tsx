6ca10e2a26460d67fac07cc46d2ffc28fd7ae7fe
edxen <edabogajr@gmail.com>
2023-12-26 14:56:42 +0800

    simplify player move in HandlePlayerAction

--- components/logic/HandleTurn.tsx
+++ components/logic/HandleTurn.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { THandleGameProps } from './HandleGame';
 import GetData from '../hooks/GetData';
-import { TPlayer } from '../reducers/initialStates';
+import { TPlayer, TTile } from '../reducers/initialStates';
 import { setDice } from '../reducers/diceReducer';
-import { setPlayer } from '../reducers/playersReducer';
-import { updatePhase } from '../reducers/gameReducer';
+import { setPlayer, setPlayers } from '../reducers/playersReducer';
+import { updateGame, updatePhase } from '../reducers/gameReducer';
 import config from '../config';
+import { cp } from 'fs';
+import { setTile } from '../reducers/tilesReducer';
 
 const HandlePlayerActionChoice = ({ dispatch, queue, player }: { dispatch: THandleGameProps['dispatch'], queue: string[], player: TPlayer; }) => {
     const handleComputerTurn = () => {
@@ -31,7 +33,7 @@ const HandleDiceRoll = ({ dispatch, player }: { dispatch: THandleGameProps['disp
                 count++;
             } else {
                 clearInterval(rollingInterval);
-                dispatch(setPlayer({ id: player.id, last_path: player.path, roll: force ?? randomize() }));
+                dispatch(setPlayer({ id: player.id, last_path: player.path, roll: force ? force : randomize() }));
                 dispatch(updatePhase({ phase: 'action' }));
             }
         }, config.rollSpeed || 150);
@@ -42,17 +44,52 @@ const HandleDiceRoll = ({ dispatch, player }: { dispatch: THandleGameProps['disp
     diceRoll();
 };
 
-const HandlePlayerActions = () => {
-    // console.log('handling player movement');
-    // Handle player movements after determining dice roll
-    // Handle additional actions related to movement as well: ex. portal warping
+interface THandlePlayerActions {
+    dispatch: THandleGameProps['dispatch'];
+    players: TPlayer[];
+    tiles: TTile[];
+    playerId: string;
+    player: Required<TPlayer>,
+    playerTile: TTile,
+    getTile: ({ path }: { path: number; }) => TTile;
+}
+
+const HandlePlayerActions = ({ dispatch, players, tiles, playerId, player, playerTile, getTile }: THandlePlayerActions) => {
+    const moveToNextTile = (playerTile: TTile, moveCounter: number) => {
+        const updateTileOccupants = (index: number, occupants: string[]) => dispatch(setTile({ index, key: 'occupants', value: occupants }));
+
+        const nextPath = playerTile.path + moveCounter;
+        const lastTile = getTile({ path: nextPath - 1 });
+        const nextTile = getTile({ path: nextPath });
+
+        updateTileOccupants(nextTile.index, [playerId, ...nextTile.occupants]);
+        updateTileOccupants(lastTile.index, lastTile.occupants.filter(id => id !== playerId));
+    };
+
+    const actionSequence = () => {
+    };
+
+    let moveCounter = 0;
+    const actionInterval = setInterval(() => {
+        if (moveCounter !== player.roll) {
+            moveToNextTile(playerTile, moveCounter + 1);
+            moveCounter++;
+        } else {
+            clearInterval(actionInterval);
+            actionSequence();
+        }
+    }, config.moveSpeed || 150);
+
+    actionInterval;
 };
 
 const HandleTurn = ({ dispatch, game, players, tiles, dice }: THandleGameProps) => {
     const { round } = game;
     const { phase, queue } = round;
-    const { getPlayerData } = GetData();
+    const { getPlayerData, getPlayerTile, getTile } = GetData();
     const player = getPlayerData(queue[0]);
+    const playerTile = getPlayerTile(queue[0]);
+    const playerId = queue[0] ?? '';
 
     useEffect(() => {
         switch (phase) {
@@ -63,7 +100,7 @@ const HandleTurn = ({ dispatch, game, players, tiles, dice }: THandleGameProps) 
                 HandleDiceRoll({ dispatch, player });
                 break;
             case 'action':
-                HandlePlayerActions();
+                HandlePlayerActions({ dispatch, players, tiles, playerId, player, playerTile, getTile });
                 break;
         }
     }, [phase]);
