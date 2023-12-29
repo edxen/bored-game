3806de81e90fb1747b92b46f508d205eba38d771
edxen <edabogajr@gmail.com>
2023-12-29 11:32:42 +0800

    add back button to main menu

--- components/game/Menu.tsx
+++ components/game/Menu.tsx
@@ -51,9 +51,13 @@ const Menu = () => {
 
     const [start, setStart] = useState(false);
 
+    const handleBack = () => {
+        setNav('menu');
+    };
+
     const handleStart = () => {
         if (nav === 'menu') {
-            setNav('start');
+            setNav('start'); ``;
         } else {
             let availablePath = Array.from({ length: 4 }).map((_, i) => getSameSideColumn(i, 1)).sort(() => Math.random() - 0.5);
             setPlayers(prevPlayers => prevPlayers.map((prevPlayer, i) => ({ ...prevPlayer, path: availablePath[i] })));
@@ -91,15 +95,19 @@ const Menu = () => {
         }
     }, [start]);  // eslint-disable-line react-hooks/exhaustive-deps
 
+    const buttonClass = 'px-4 py-2 bg-white text-black font-bold border rounded-md hover:bg-slate-100 cursor-pointer whitespace-nowrap';
+    const startButtonClass = (nav === 'start' && players.length < 2) ? 'hidden' : '';
+
     return (
         <div className={`flex flex-col justify-center items-center ${nav === 'menu' ? 'h-full' : ''}`}>
             <div className={`transition-all delay-250 ${nav === 'start' ? 'w-full p-4 mb-2 border rounded-md bg-black text-white' : 'w-0'}`}>
-                <div className='flex justify-center items-center w-full'>
+                <div className='flex justify-center items-center w-full gap-2'>
                     <div className={`transition-all delay-250 font-bold ${nav === 'start' ? 'visible opacity-100 px-4 py-2 flex-grow' : 'invisible opacity-0 w-0'}`}>Players</div>
                     {
-                        players.length > 1 &&
-                        <div className='px-4 py-2 bg-white text-black font-bold border rounded-md hover:bg-slate-100 cursor-pointer whitespace-nowrap' onClick={handleStart}>Start Game</div>
+                        nav === 'start' &&
+                        <div className={buttonClass} onClick={handleBack}>Back</div>
                     }
+                    <div className={`${buttonClass} ${startButtonClass}`} onClick={handleStart}>Start Game</div>
                 </div>
             </div>
             <div className={`flex justify-center items-flex-start w-full gap-4 transition-height duration-500 ${nav === 'start' ? 'delay-150 visible opacity-100 h-full' : 'delay-0 invisible opacity-0 h-0'}`}>
