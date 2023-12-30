970d2d4b62603f48b3a888f56ed33b4bb582d16b
edxen <edabogajr@gmail.com>
2023-12-30 18:23:04 +0800

    add backround color to flags icon in player queue

--- components/game/TurnUI.tsx
+++ components/game/TurnUI.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import GetData from "@/components/hooks/GetData";
+import { TTile } from "../reducers/initialStates";
 
 const TurnUI = () => {
-    const { game, queue, getPlayerData, getTile } = GetData();
+    const { game, queue, tiles, getPlayerData, getTile } = GetData();
     const { round } = game;
+
+    const getTileBG = (currentTile: TTile) => {
+        if (currentTile.type === 'flag') {
+            const flagPaths = tiles.filter(tile => tile.type === 'flag');
+            let obtainableFlag = 'bg-slate-200';
+            flagPaths.forEach((tile, i) => {
+                if (currentTile.path === tile.path) obtainableFlag = game.flags[i];
+            });
+            return obtainableFlag;
+        }
+    };
     return (
         <div className="flex flex-col gap-2 justify-center items-center">
             <div className="font-bold">
@@ -18,7 +30,7 @@ const TurnUI = () => {
                                     {getPlayerData(id).name}
                                 </div>
 
-                                <Image src={`/images/icons/icon-${getTile({ path: getPlayerData(id).path }).type}.png`} alt={id} width="14" height="14" className="bg-slate-200 h-auto w-auto rounded-full" />
+                                <Image src={`/images/icons/icon-${getTile({ path: getPlayerData(id).path }).type}.png`} alt={id} width="14" height="14" className={`${getTileBG(getTile({ path: getPlayerData(id).path }))} h-auto w-auto rounded-full`} />
                             </div>
                             {i !== queue.length - 1 && (
                                 <Image src="/images/icons/icon-arrow-left.png" alt={id} width="10" height="10" className="h-auto w-auto" />
